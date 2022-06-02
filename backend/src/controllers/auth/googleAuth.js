f1cc5d216bb516d980782af5ae6aec7aa46e9dec
ZjeniiaSt <zeniiastupjak@gmail.com>
2022-06-02 18:57:05 +0200

    data

--- backend/src/controllers/auth/googleAuth.js
+++ backend/src/controllers/auth/googleAuth.js
@@ -3,7 +3,7 @@ const queryString = require('query-string');
 const googleAuth = async (req, res) => {
   const stringifiedParams = queryString.stringify({
     client_id: process.env.GOOGLE_CLIENT_ID,
-    redirect_uri: `${process.env.BASE_URL}/auth/google-redirect`,
+    redirect_uri: `${process.env.BASE_URL}/api/users/google-redirect`,
     scope: [
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
@@ -12,6 +12,7 @@ const googleAuth = async (req, res) => {
     access_type: 'offline',
     prompt: 'consent',
   });
+
   return res.redirect(
     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
   );
