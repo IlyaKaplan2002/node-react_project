b431c81910799a20d364b831ec3311b475e8ae67
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-06-03 21:45:36 +0300

    before merging

--- backend/src/controllers/auth/logout.js
+++ backend/src/controllers/auth/logout.js
@@ -4,7 +4,9 @@ const { createResponse } = require('../../helpers');
 const logout = async (req, res) => {
   const { _id: id } = req.user;
   await userService.updateToken(id, null);
-  res.json(createResponse(204, { message: 'Logged out successfully' }));
+  res
+    .status(204)
+    .json(createResponse(204, { message: 'Logged out successfully' }));
 };
 
 module.exports = logout;
