2bd102ef22a4267f5fe01f47e52533654a50ecfc
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-06-05 01:27:19 +0300

    some fixes

--- backend/src/models/training.js
+++ backend/src/models/training.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
+const { startOfDay } = require('date-fns');
 
 const add = Joi.object({
-  start: Joi.date().required(),
+  start: Joi.date().required().min(startOfDay(Date.now())),
   end: Joi.date().required().greater(Joi.ref('start')),
   books: Joi.array().items(Joi.string()).allow('').required(),
 });
