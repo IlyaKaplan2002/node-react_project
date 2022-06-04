cd83536926179a64c49cda9b08d133ead6188264
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-06-04 11:35:30 +0300

    before merging

--- frontend/src/components/utils/Button/Button.js
+++ frontend/src/components/utils/Button/Button.js
@@ -1,8 +1,23 @@
-import React from "react";
-import StyledButton from './Button.styled'
+import React from 'react';
+import StyledButton from './Button.styled';
 
-const Button = ({ label, type, className, onClick }) => {
-    return <StyledButton type={type} className={ className } onClick={onClick}>{ label }</StyledButton>;
+const Button = ({
+  label,
+  type = 'button',
+  className,
+  onClick,
+  filled = false,
+}) => {
+  return (
+    <StyledButton
+      type={type}
+      className={className}
+      onClick={onClick}
+      filled={filled}
+    >
+      {label}
+    </StyledButton>
+  );
 };
 
 export default Button;
--- frontend/src/components/utils/Button/Button.styled.js
+++ frontend/src/components/utils/Button/Button.styled.js
@@ -1,19 +1,25 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-    text-transform: capitalize;
-    padding: 12px 76px;
-    font-size: 16px;
-    font-family: ${props => props.theme.fonts.families.montserrat};
-    font-style: ${props => props.theme.fonts.weights.semiBold};
-    line-height: 1.25;
-    border: none;
-    background-color: ${props => props.theme.colors.mainOrange};
-    color: ${props => props.theme.colors.mainWhite};
+  text-transform: capitalize;
+  padding: 12px;
+  min-width: 100px;
+  font-size: 16px;
+  font-family: ${props => props.theme.fonts.families.montserrat};
+  font-style: ${props => props.theme.fonts.weights.semiBold};
+  line-height: 1.25;
+  border: ${props =>
+    props.filled
+      ? props.theme.borders.transparent
+      : props.theme.borders.button};
+  background-color: ${props =>
+    props.filled ? props.theme.colors.mainOrange : 'transparent'};
+  color: ${props =>
+    props.filled ? props.theme.colors.mainWhite : props.theme.colors.mainBlack};
 
-    &:hover {
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default Button;
