f0bd0d3eaa7d1c62b0d3b5203beb41fda2ba0e36
Korotenko Alex <alexey.korotenko90@gmail.com>
2022-06-06 20:31:58 +0300

    some fix

--- frontend/src/views/LoginView.js
+++ frontend/src/views/LoginView.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import NonAuthHeader from 'components/NonAuthHeader';
 import LoginForm from 'components/Forms/FormLogin/LoginForm';
 import { Container } from 'styles';
@@ -6,14 +6,6 @@ import Quote from 'components/Quote';
 import { Wrapper } from 'components/Forms/AuthForm.styled';
 
 const LoginView = () => {
-  const [showModal, setShowModal] = useState(false);
-  useEffect(() => {
-    document.title = 'Books Reading Login';
-  }, []);
-
-  const onToggleModal = img => {
-    setShowModal(prevState => !prevState);
-  };
   return (
     <>
       <Container>
