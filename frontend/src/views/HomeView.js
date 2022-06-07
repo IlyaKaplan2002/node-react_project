7335d12c2d6fae6f4558b0d4770645b0f82ef0dd
NadiiaHonchar <17nmg82@gmail.com>
2022-06-07 17:04:15 +0300

    fixed

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import HelpInfoMobile from 'components/HelpInfoMobile';
 import NonAuthHeader from 'components/NonAuthHeader';
-import StartTrainingEmptyMobile from '../components/StartTraining';
-
 
 const HomeView = () => {
   return (
     <>
-      {/* <NonAuthHeader />
-      <HelpInfoMobile />       */}
-      
-      <StartTrainingEmptyMobile></StartTrainingEmptyMobile>
+      <NonAuthHeader />
+      <HelpInfoMobile />      
     </>
   );
 };
