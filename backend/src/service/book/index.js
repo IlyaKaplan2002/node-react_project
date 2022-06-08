011ebcaf10c97fad751a494955950456fdd1fa33
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-06-08 23:34:43 +0300

    --amend
    before merging

--- backend/src/service/book/index.js
+++ backend/src/service/book/index.js
@@ -2,9 +2,12 @@ const addBook = require('./addBook');
 const updateBook = require('./updateBook');
 const findBooksByUserId = require('./findBooksByUserId');
 const findBooksByIdAndRemove = require('./findBooksByIdAndRemove');
+const findBook = require('./findBook');
+
 module.exports = {
   addBook,
   updateBook,
   findBooksByUserId,
+  findBook,
   findBooksByIdAndRemove,
 };
