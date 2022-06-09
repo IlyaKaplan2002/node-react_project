1c2db29aee3b7a9d89bd7115ab44fb41e4fae4cf
ZjeniiaSt <zeniiastupjak@gmail.com>
2022-06-09 07:29:37 +0200

    res_done

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import getInfo from 'api/auth/getInfo';
 import { notifyError } from 'helpers';
 import React, { useCallback, useEffect } from 'react';
-import Chart from './Chart';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { authActions, authSelectors } from 'redux/auth';
 import Router from './Router';
@@ -25,7 +25,7 @@ const App = () => {
     }
   }, [token, getUserData]);
 
-  return <Chart />;
+  return <Router />;
 };
 
 export default App;
