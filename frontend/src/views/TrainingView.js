03ad3c22c27c8c0d6cb95c4b77b55a51cdea0403
NadiiaHonchar <17nmg82@gmail.com>
2022-06-11 00:09:00 +0300

    fixed

--- frontend/src/views/TrainingView.js
+++ frontend/src/views/TrainingView.js
@@ -1,12 +1,10 @@
 import AppBar from 'components/AppBar';
 import React from 'react';
-import Done from 'components/Done';
 
 const TrainingView = () => {
   return (
     <>
       <AppBar />
-      <Done></Done>
     </>
   );
 };
