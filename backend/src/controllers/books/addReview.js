756c158c4329fb81cc946167d8c43250300e30d7
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2022-06-11 18:27:53 +0200

    added second version of documentation

--- backend/src/controllers/books/addReview.js
+++ backend/src/controllers/books/addReview.js
@@ -5,7 +5,7 @@ const addReview = async (req, res) => {
   const { _id: userId } = req.user;
   const { bookId } = req.params;
   const book = await bookService.findBook({ _id: bookId, owner: userId });
-  if (!book) throwError('Boook not found', 404);
+  if (!book) throwError('Book not found', 404);
   const newBook = await bookService.updateBook(bookId, req.body);
   res.status(201).json(createResponse(201, { book: newBook }));
 };
