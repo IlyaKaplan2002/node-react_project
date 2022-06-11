45088a2f2135213501f7c9ddeb840881b6c7a6ac
ZjeniiaSt <zeniiastupjak@gmail.com>
2022-06-11 09:09:27 +0200

    labels done +  fix amount of  days

--- frontend/src/components/Chart/Chart.jsx
+++ frontend/src/components/Chart/Chart.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import ChartStyled from './Chart.styled';
 import Days from './Days';
 import options from './chartOptions';
-import data from './chartDate';
+//import data from './chartDate';
+import { getStatistic } from 'api/statistics';
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
+import { getDays } from './helpers';
+import { statisticsActions } from 'redux/statistics';
 
 import {
   Chart as ChartJS,
@@ -27,10 +32,57 @@ ChartJS.register(
 );
 
 const Chart = () => {
+  const [amountDays, setAmountDays] = useState();
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'PLAN',
+        fill: false,
+        data: [0, 2, 3, 4, 5, 6, 5, 4], //from back
+        borderColor: 'rgba(9, 30, 63, 1,)',
+        backgroundColor: 'rgba(9, 30, 63, 1)',
+        borderWidth: 2,
+        tension: 0.5,
+      },
+      {
+        label: 'ACT',
+        fill: false,
+        data: [1, 2, 6, 8, 5, 6, 3, 2], //from back
+        borderColor: 'rgba(255, 107, 8, 1)',
+        backgroundColor: 'rgba(255, 107, 8, 1)',
+        borderWidth: 2,
+        tension: 0.5,
+      },
+    ],
+  });
+
+  const token = useSelector(authSelectors.getToken);
+  const dispatch = useDispatch();
+
+  const fetchAllDays = useCallback(async () => {
+    try {
+      const { statistic } = await getStatistic(token);
+      dispatch(statisticsActions.init(statistic));
+      console.log(statistic);
+      const labels = getDays(statistic);
+      setAmountDays(labels.length);
+      setData({
+        labels: labels,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [token, dispatch]);
+
+  useEffect(() => {
+    fetchAllDays();
+  }, [fetchAllDays]);
+
   return (
     <>
       <ChartStyled>
-        <Days />
+        <Days>{amountDays}</Days>
         <Line data={data} options={options} />
       </ChartStyled>
     </>
--- frontend/src/components/Chart/Days.jsx
+++ frontend/src/components/Chart/Days.jsx
@@ -1,35 +1,9 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { getStatistic } from 'api/statistics';
-import { useDispatch, useSelector } from 'react-redux';
-import { authSelectors } from 'redux/auth';
 import DaysStyled from './Days.styled';
-import { statisticsActions } from 'redux/statistics';
-import { getDays } from './helpers';
 
-const Days = () => {
-  const [days, setDays] = useState();
-
-  const token = useSelector(authSelectors.getToken);
-  const dispatch = useDispatch();
-
-  const fetchAllBooks = useCallback(async () => {
-    try {
-      const { statistic } = await getStatistic(token);
-      dispatch(statisticsActions.init(statistic));
-      const dates = getDays(statistic).length;
-      setDays(dates);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [token, dispatch]);
-
-  useEffect(() => {
-    fetchAllBooks();
-  }, [fetchAllBooks]);
-
+const Days = props => {
   return (
     <DaysStyled>
-      <p>{days}</p>
+      <p> {props.children}</p>
     </DaysStyled>
   );
 };
