c0b878e4ebd2c9e2751f97a80763aa3b636961f5
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-06-19 20:10:46 +0300

    overflow hiddden added

--- frontend/src/styles/Container.styled.js
+++ frontend/src/styles/Container.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin: 0 auto;
+  overflow: hidden;
   width: ${props => props.theme.breakpoints.mobile};
 
   @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
